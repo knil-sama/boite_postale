c54547adce90c33b4622960d017a16bf56f54590
knil-sama <cols.knil@gmail.com>
2017-02-03 01:17:41 +0100

    v0.1 of the project, static page on aws

---
+++ README.md
@@ -0,0 +1,5 @@
+# boite-postale
+
+Simple personal project trying to use open date from laposte 
+
+with aws, folium tech
---
+++ generate_map.py
@@ -0,0 +1,30 @@
+"""
+    Take a post-office box file and generate a Folium map 
+"""
+# -*- coding: utf-8 -*-
+
+from pandas import DataFrame, read_csv
+import folium
+from io import open
+from tqdm import tqdm
+
+def main():
+    """
+        Main function
+        1) Open boite_postale.csv file
+        2) Convert to DataFrame
+        3) Create folium object
+        4) Create and add marker
+        5) Generate map
+    """
+    with open("boite_postale.csv",encoding="utf-8") as boite_postale_file:
+        boite_postale_dataframe = read_csv(boite_postale_file,sep=";")
+    map_folium = folium.Map(location=[46.6025,1.9053], zoom_start=12)
+    for index,row in tqdm(boite_postale_dataframe.iterrows(),total=len(boite_postale_dataframe)):
+        folium.Marker(location=row["latlong"].split(","),popup=row["co_mup"]).add_to(map_folium)
+    map_folium.save("index.html")
+
+
+if __name__ == "__main__":
+    main()
+
