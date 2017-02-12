cd79443564666f904074b9630a89634693f6b103
knil-sama <cols.knil@gmail.com>
2017-02-12 17:06:03 +0100

    Use marker cluster so at least the map can be displayed using aws static website, still too long to load

--- generate_map.py
+++ generate_map.py
@@ -20,8 +20,9 @@ def main():
     with open("boite_postale.csv",encoding="utf-8") as boite_postale_file:
         boite_postale_dataframe = read_csv(boite_postale_file,sep=";")
     map_folium = folium.Map(location=[46.6025,1.9053], zoom_start=12)
+    marker_cluster = folium.MarkerCluster().add_to(map_folium)
     for index,row in tqdm(boite_postale_dataframe.iterrows(),total=len(boite_postale_dataframe)):
-        folium.Marker(location=row["latlong"].split(","),popup=row["co_mup"]).add_to(map_folium)
+        folium.Marker(location=row["latlong"].split(","),popup=row["co_mup"]).add_to(marker_cluster)
     map_folium.save("index.html")
 
 
