6c30780f7c6e353223d3d7a03eebde1438f82a40
knil-sama <cols.knil@gmail.com>
2017-05-10 01:41:16 +0200

    Update generate_map.py
    Handle missing value for "lalong" field and restrain on Paris data

--- generate_map.py
+++ generate_map.py
@@ -3,7 +3,7 @@
 """
 # -*- coding: utf-8 -*-
 
-from pandas import DataFrame, read_csv
+from pandas import DataFrame, read_csv, isnull
 import folium
 from io import open
 from tqdm import tqdm
@@ -19,9 +19,13 @@ def main():
     """
     with open("boite_postale.csv",encoding="utf-8") as boite_postale_file:
         boite_postale_dataframe = read_csv(boite_postale_file,sep=";")
+    #restrain on Paris area for optimisation of display
+    boite_postale_dataframe = boite_postale_dataframe[boite_postale_dataframe["co_postal"].str.startswith("75")]
     map_folium = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
     for index,row in tqdm(boite_postale_dataframe.iterrows(),total=len(boite_postale_dataframe)):
-        folium.Marker(location=row["Latlong"].split(","),popup=row["CO_MUP"]).add_to(map_folium)
+        #some value can be missing
+        if(isnull(row["latlong"]) == False):
+            folium.Marker(location=row["latlong"].split(","),popup=row["co_mup"]).add_to(map_folium)
     map_folium.save("index.html")
 
 
