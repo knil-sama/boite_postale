15286866f1ccb137a8611038acccbb86da843278
knil-sama <cols.knil@gmail.com>
2017-06-06 13:58:01 +0200

    Update generate_map.py
    Add image for marker

--- generate_map.py
+++ generate_map.py
@@ -19,6 +19,7 @@ def main():
     """
     with open("boite_postale.csv",encoding="utf-8") as boite_postale_file:
         boite_postale_dataframe = read_csv(boite_postale_file,sep=";",dtype="str")
+    icon = folium.features.CustomIcon("small_boite_aux_lettres_cropped.png")
     #restrain on Paris area for optimisation of display
     boite_postale_dataframe = boite_postale_dataframe[boite_postale_dataframe["CO_POSTAL"].str.startswith("75")]
     boite_postale_dataframe.fillna(value = {"VA_NO_VOIE":"","LB_EXTENSION":"","LB_VOIE_EXT":""},inplace=True)
@@ -29,7 +30,7 @@ def main():
         if(isnull(row["Latlong"]) == False):
             address = " ".join([row["VA_NO_VOIE"],row["LB_EXTENSION"],row["LB_VOIE_EXT"]])
             content_popup = "adresse : %s" % address
-            folium.Marker(location=row["Latlong"].split(","),popup=content_popup).add_to(marker_cluster)
+            folium.Marker(location=row["Latlong"].split(","),icon=icon,popup=content_popup).add_to(marker_cluster)
     map_folium.add_child(marker_cluster)
     map_folium.save("build/index.html")
 
