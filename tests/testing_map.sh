298437bbfe8f6552505868f1a6601451dc408de8
knil-sama <cols.knil@gmail.com>
2017-06-06 14:55:02 +0200

    Update testing_map.sh
    try to augment limit of test now that we added custom images for marker

--- tests/testing_map.sh
+++ tests/testing_map.sh
@@ -3,7 +3,7 @@
 testSizeMaxMap()
 {
   file_size_kb=`du -k "build/index.html" | cut -f1`
-  assertTrue "[ $file_size_kb -lt 1800 ]"
+  assertTrue "[ $file_size_kb -lt 2800 ]"
 }
 
 . shunit2-2.1.6/src/shunit2
