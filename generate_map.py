1c41e4fe9cab01ea751bb42e807705460a48b7c7
knil-sama <cols.knil@gmail.com>
2017-06-06 14:45:17 +0200

    Update generate_map.py
    Correct icon creation https://github.com/python-visualization/folium/issues/377 and use url for image instead of relative path

--- generate_map.py
+++ generate_map.py
@@ -19,7 +19,6 @@ def main():
     """
     with open("boite_postale.csv",encoding="utf-8") as boite_postale_file:
         boite_postale_dataframe = read_csv(boite_postale_file,sep=";",dtype="str")
-    icon = folium.features.CustomIcon("small_boite_aux_lettres_cropped.png",icon_size=(14, 14))
     #restrain on Paris area for optimisation of display
     boite_postale_dataframe = boite_postale_dataframe[boite_postale_dataframe["CO_POSTAL"].str.startswith("75")]
     boite_postale_dataframe.fillna(value = {"VA_NO_VOIE":"","LB_EXTENSION":"","LB_VOIE_EXT":""},inplace=True)
@@ -30,6 +29,8 @@ def main():
         if(isnull(row["Latlong"]) == False):
             address = " ".join([row["VA_NO_VOIE"],row["LB_EXTENSION"],row["LB_VOIE_EXT"]])
             content_popup = "adresse : %s" % address
+            #need to create a different icon for each marker
+            icon = folium.features.CustomIcon(https://github.com/knil-sama/boite_postale/blob/master/small_boite_aux_lettres_cropped.png,icon_size=(14, 14))
             folium.Marker(location=row["Latlong"].split(","),icon=icon,popup=content_popup).add_to(marker_cluster)
     map_folium.add_child(marker_cluster)
     map_folium.save("build/index.html")
